4ed57ea92d12d57af39bc7da83c5da109a289619
Korisnik <vrafaeli@msn.com>
2017-01-05 22:50:55 +0100

    Temporary comment DaoConfig

--- src/main/java/org/evedraf/examples/spring/config/DaoConfig.java
+++ src/main/java/org/evedraf/examples/spring/config/DaoConfig.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class DaoConfig {
 
-    @Bean
+   /* @Bean
     SessionFactory getSessionFactory(){
 
         LocalSessionFactoryBuilder sessionFactory = new LocalSessionFactoryBuilder(getDataSource());
@@ -27,9 +27,9 @@ public class DaoConfig {
         properties.put("dialect", "org.hibernate.dialect.HSQLDialect");
 
         properties.put("hibernate.show_sql", "true");
-        /*properties.put("hibernate.hbm2ddl.auto", "update");
+        *//*properties.put("hibernate.hbm2ddl.auto", "update");
         properties.put("hibernate.generate_statistics", "true");
-        */
+        *//*
 
         sessionFactory.addProperties(properties);
 
@@ -49,12 +49,12 @@ public class DaoConfig {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
-        dataSource.setUrl("jdbc:hsqldb:hsql://localhost:9001");
+        dataSource.setUrl("postgres://jjxrrhmuvwdsdy:c611504c353a2240a099a1ce47da35e4fd670c5ca5f82960d28847b05cd24147@ec2-107-20-149-243.compute-1.amazonaws.com:5432/d65670563i6thc");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
 
         return dataSource;
-    }
+    }*/
 
 
 }
