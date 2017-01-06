f431ff0bdb356dc7815d57f77502324e64888eaa
Korisnik <vrafaeli@msn.com>
2017-01-06 20:54:36 +0100

    Drawing circle on canvas on click.

--- src/main/webapp/static/battleScreen.js
+++ src/main/webapp/static/battleScreen.js
@@ -40,7 +40,7 @@ window.onload = function() {
         var y = event.pageY - canvas.offsetTop;
 
         ctx.beginPath();
-        ctx.arc(100, 100, 5, 0, 2*Math.PI);
+        ctx.arc(x, y, 5, 0, 2*Math.PI);
         ctx.stroke();
     }
 };
