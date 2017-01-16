7a28426c0db62093452c206b0cb77b7071f2bcda
evedraf <vrafaeli@msn.com>
2017-01-16 15:55:19 +0100

    Removing current testing aspect.

--- src/main/java/org/evedraf/examples/spring/aspect/MySimpleAspect.java
+++ src/main/java/org/evedraf/examples/spring/aspect/MySimpleAspect.java
@@ -13,10 +13,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class MySimpleAspect {
 
-    @Before("execution(public * org.evedraf.examples.spring.business.playerSettings.PlayerLogic.*(..))")
+    /*@Before("execution(public * org.evedraf.examples.spring.business.playerSettings.PlayerLogic.*(..))")
     public void loggingAspect(JoinPoint joinPoint){
 
 
         System.out.println("Logging Aspect: PlayerLogic's method " + joinPoint.getSignature().toString() + " started.");
-    }
+    }*/
 }
